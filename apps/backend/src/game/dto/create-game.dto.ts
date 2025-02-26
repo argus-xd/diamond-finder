180888f19594f7ff9cf58dee45807e4fb5f81b43
Sergey Sokolov <xxxARGUSxxx58@gmail.com>
2025-02-26 16:44:51 +0500

    feat: add dto CreateGameDto

---
+++ apps/backend/src/game/dto/create-game.dto.ts
@@ -0,0 +1,18 @@
+import { IsNumber, Min, Max } from 'class-validator';
+
+export class CreateGameDto {
+  @IsNumber()
+  @Min(3)
+  @Max(15)
+  rows: number;
+
+  @IsNumber()
+  @Min(3)
+  @Max(15)
+  cols: number;
+
+  @IsNumber()
+  @Min(1)
+  @Max(15)
+  diamonds: number;
+}
--- apps/backend/src/game/game.controller.ts
+++ apps/backend/src/game/game.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { GameService } from './game.service';
+import { CreateGameDto } from './dto/create-game.dto';
 
 @Controller('game')
 export class GameController {
   constructor(private readonly gameService: GameService) {}
 
   @Post('create')
-  async createGameSession(@Body() createGameDto: { rows: number; cols: number, diamonds: number }) {
+  async createGameSession(@Body() createGameDto: CreateGameDto) {
     return await this.gameService.createGameSession(createGameDto.rows, createGameDto.cols, createGameDto.diamonds);
   }
 }
